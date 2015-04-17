fb7b2721b76b4f7b65a0c9f3530c3a6f47323d38
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-04-17 15:09:37 +0100

    Remove old OSPFUnnumbered packages

--- pkgs/workbench/cldemo-wbench-ospfunnum-base-chef/debian/usr/local/share/chef/cookbooks/cldemo_ospf_unnumbered/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'cldemo_ospf_unnumbered'
-maintainer       'Cumulus Networks'
-maintainer_email 'mwillsher@cumulusnetworks.com'
-license          'All rights reserved'
-description      'Configures Quagga OSPF unnumbered demo'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
