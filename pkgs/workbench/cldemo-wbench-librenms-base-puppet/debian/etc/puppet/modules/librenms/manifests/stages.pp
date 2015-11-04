cf9663222d2df4238a5cdc71ee0aef4f744c7036
Leslie Carr <geekgirl@gmail.com>
2015-11-04 11:59:52 -0800

    removed many old unused demo's

--- pkgs/workbench/cldemo-wbench-librenms-base-puppet/debian/etc/puppet/modules/librenms/manifests/stages.pp
+++
@@ -1,3 +0,0 @@
-stage { 'last':
-  after => Stage['main']
-}
--- pkgs/workbench/cldemo-wbench-openstack-2lt22s-puppet/debian/etc/puppet/modules/openstack/files/admin-openrc.sh
+++
@@ -1,4 +0,0 @@
-export OS_USERNAME=admin
-export OS_TENANT_NAME=admin
-export OS_AUTH_URL=http://192.168.0.201:35357/v2.0
-export OS_REGION_NAME=regionOne
